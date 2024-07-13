674ce534ac06e395f7cca725fc6b6920def900a9
Shubel Islam Sunny <shubelislam.diu@gmail.com>
2024-07-13 13:27:32 +0600

    Icon Button added

--- lib/main.dart
+++ lib/main.dart
@@ -37,20 +37,90 @@ class Home extends StatelessWidget {
         //   size: 50,
         //   color: Colors.white,
         // ),
+
         // child: Image.network(
         //     'https://img.freepik.com/free-photo/professional-soccer-player-cartoon_23-2151502421.jpg?ga=GA1.1.810212258.1715444724&semt=sph'),
+
         // child: Image.asset(
         //   'assets/images/Football.webp',
         //   width: 300,
         //   height: 150,
         //   fit: BoxFit.contain,
         // ),
-        child: ElevatedButton(
+
+        // Elevated Button
+        // child: ElevatedButton(
+        //   onPressed: () {
+        //     print('Button Pressed');
+        //   },
+        //   child: Text('Click'),
+        // ),
+
+        // Icon Button
+        child: IconButton(
           onPressed: () {
-            print('Button Pressed');
+            print('Adding an icon');
           },
-          child: Text('Click'),
+          icon: Icon(
+            // Icons.add_a_photo,
+            // color: Colors.white,
+            Icons.add, color: Colors.white,
+          ),
         ),
+
+        // Text Button
+        // child: TextButton(
+        //   onPressed: () {
+        //     print('Show Button Pressed');
+        //   },
+        //   child: Text(
+        //     'Text Button',
+        //     style: TextStyle(color: Colors.white),
+        //   ),
+        // ),
+
+        // Gesture Detector with image
+        // child: GestureDetector(
+        //   onTap: (){
+        //     print('A single Tap');
+        //   },
+        //   onDoubleTap: (){
+        //     print('A double Tap');
+        //   },
+        //   child: Image.asset(
+        //     'assets/images/Football.webp',
+        //     fit: BoxFit.cover,
+        //   ),
+        // ),
+
+        // // Gesture Detector with text
+        // child: GestureDetector(
+        //   onTap: () {
+        //     print('A single Tap');
+        //   },
+        //   onDoubleTap: () {
+        //     print('A double Tap');
+        //   },
+        //   child: Text(
+        //     'Normal Text',
+        //     style: TextStyle(color: Colors.black),
+        //   ),
+        // ),
+
+        // Ink Well with text
+        // child: InkWell(
+        //   onTap: () {
+        //     print('A single Tap');
+        //   },
+        //   onDoubleTap: () {
+        //     print('A double Tap');
+        //   },
+        //   child: Text(
+        //     'Normal Text',
+        //     style: TextStyle(color: Colors.black),
+        //   ),
+        // ),
+
       ),
     );
   }
